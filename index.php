be8b7f2f7c58e28ce0c84e5dd1a7a1e876191d51
Satir666 <43719494+Satir666@users.noreply.github.com>
2024-04-21 19:04:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
 
-$servername = "sql11.freemysqlhosting.net";
-$username = "sql11700649";
-$password = "KMsCLmwY7u";
-$dbname = "sql11700649";
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $ID = $_POST['name1'];
   $Name = $_POST['name2'];
   $Subname = $_POST['name3'];
 
+  $servername = "sql11.freemysqlhosting.net";
+  $username = "sql11700649";
+  $password = "KMsCLmwY7u";
+  $dbname = "sql11700649";
   $con = mysqli_connect($servername, $username, $password, $dbname);
   if(mysqli_connect_errno)  
   {
