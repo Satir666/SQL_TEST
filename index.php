d2ea243032c464bfeb4a183cffb2f75412cb924c
Satir666 <43719494+Satir666@users.noreply.github.com>
2024-04-21 18:53:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,19 @@
 <?php
+
 $servername = "sql11.freemysqlhosting.net";
 $username = "sql11700649";
 $password = "KMsCLmwY7u";
 $dbname = "sql11700649";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $ID = $_POST['name1'];
   $Name = $_POST['name2'];
   $Subname = $_POST['name3'];
-  // Ваш код здесь...
-  // Параметры подключения к базе данных
-  // Создаем соединение с базой данных
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  // Подготовка запроса
-  $stmt = $conn->prepare("INSERT INTO table_name (name, email, message) VALUES (?, ?, ?)");
-  $stmt->bind_param("sss", $name, $email, $message);
 
-  // Выполнение запроса
-  $stmt->execute();
-
-  // Закрываем соединение
-  $stmt->close();
-  $conn->close();
+  $con = new mysqli_connect($servername, $username, $password, $dbname);
+  if(mysqli_connect_errno)  
+  {
+    print "<p style='color: red'>Произошла ошибка!</p>";
+  }
 }
 ?>
