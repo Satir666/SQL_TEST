2ad7bf7d9580b999f85a761f4c7c0a0498a761aa
Satir666 <43719494+Satir666@users.noreply.github.com>
2024-04-21 19:20:50 +0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,14 @@
+const button = document.querySelector("input");
+const paragraph = document.querySelector("p");
+
+button.addEventListener("click", updateButton);
+
+function updateButton() {
+  if (button.value === "Запустить ПК") {
+    button.value = "Выключить ПК";
+    paragraph.textContent = "ПК запущен!";
+  } else {
+    button.value = "Запустить ПК";
+    paragraph.textContent = "ПК выключен.";
+  }
+}
